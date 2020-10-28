935753c058925eab8b9807995f136d822ee91f00
rohan1229 <rs5856370@gmail.com>
2020-10-28 20:03:47 +0530

    added viewallproducts , category and view product by category

--- OnlineShopping/src/main/java/com/lti/dao/CategoryDao.java
+++ OnlineShopping/src/main/java/com/lti/dao/CategoryDao.java
@@ -1,9 +1,12 @@
 package com.lti.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import com.lti.model.Category;
 
@@ -27,5 +30,12 @@ public class CategoryDao {
 	}
 	public Category findACategory(int categoryID){
 		return em.find(Category.class, categoryID);
+	}
+	
+	public List<Category> viewAllCategories(){
+		String jpql= "select c from Category c";
+		TypedQuery<Category> query = em.createQuery(jpql, Category.class);
+		
+		return query.getResultList();
 	}
 }
--- OnlineShopping/src/main/java/com/lti/dao/ProductDao.java
+++ OnlineShopping/src/main/java/com/lti/dao/ProductDao.java
@@ -1,10 +1,14 @@
 package com.lti.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
+import com.lti.model.Category;
 import com.lti.model.Product;
 
 public class ProductDao {
@@ -25,9 +29,25 @@ public class ProductDao {
 		em.merge(product);
 		tx.commit();
 	}
+	
 	public Product findAProduct(int productId)
 	{
 		return em.find(Product.class, productId);
+	}
+	
+	public List<Product> viewAllProducts(){
+		String jpql= "select p from Product p";
+		TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+		return query.getResultList();		
+		
+	}
+	
+	public List<Product> viewProductByCategory(String category){
+		String jpql = "select p from Product p where p.category=:catg";
+		TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+		query.setParameter("catg", category);
+		
+		return query.getResultList();
 	}
 	
 }
